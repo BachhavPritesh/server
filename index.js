8a90bc0eb93af24bb3ff2e1671ad127a485f4717
BachhavPritesh <pritesh.v.bachhav.cg@gmail.com>
2026-02-04 11:53:29 +0530

    updates the subject route

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const users = [
   { id: 4, name: "RAT", role: "COOK" }
 ];
 
+const subject = [
+  {id:1, subject:"MATHS"},
+  {id:2, subject:"ENGLISH"},
+  {id:3, subject:"SCIENCE"},
+  {id:4, subject:"GEOGRAPHY"},
+  {id:5, subject:"YOGA"}
+]
+
 app.get("/", (req, res) => {
   res.send("PRITESH STARTED THE SERVER");
 });
@@ -21,6 +29,12 @@ app.get("/users",(req,res)=>{
     res.status(200).json(users);
 })
 
+app.get("/subject",(req,res)=>{
+   res.status(200).json(subject);
+});
+
+
+
 app.get("/users/:id", (req, res) => {
   const userId = Number(req.params.id);
   const user = users.find(u => u.id === userId);
@@ -31,6 +45,19 @@ app.get("/users/:id", (req, res) => {
 
   res.status(200).json(user);
 });
+
+app.get("/subject/:id",(req,res)=>{
+  const subjectId = Number(req.params.id);
+  const subjects = subject.find(s => s.id === subjectId );
+
+if(!subjects){
+  return res.status(404).json({message:"Subject not found"});
+
+}
+
+res.status(200).json(subjects);
+
+})
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
